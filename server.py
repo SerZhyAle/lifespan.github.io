83ae1d94f35b0b08b9795e63768a04f5c6fd4cea
Serhii Zhyhunenko <serzhyale@gmail.com>
2025-04-25 01:37:30 +0300

    2504250035

--- server.py
+++ server.py
@@ -6,8 +6,8 @@ from datetime import datetime
 import ssl
 
 app = Flask(__name__)
-CORS(app, resources={r"/log": {"origins": "https://serzhyale.github.io"}})
-#CORS(app, resources={r"/log": {"origins": "*"}})
+#CORS(app, resources={r"/log": {"origins": "https://serzhyale.github.io"}})
+CORS(app, resources={r"/log": {"origins": "*"}})
 
 LOG_FILE = "logs.txt"
 
